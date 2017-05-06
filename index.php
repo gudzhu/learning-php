00a0aeb1ac25b9038d52a7f269b1551d1eb29e08
gudzhu <gudzhu@users.noreply.github.com>
2017-05-06 08:35:01 +0300

    new commit

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-
-=======
-$person = [
-	'name' => 'John',
-	'age' => 28,
-	'hair' => 'brown'
-];
-
-$person['sex'] = 'man';
->>>>>>> c924b2a9912aba2d28fe19663fe970bc8edde89f
 
 require 'index.view.php';
 
--- index.view.php
+++ index.view.php
@@ -15,14 +15,6 @@
     </style>
 </head>
 <body>
-<<<<<<< HEAD
 
-=======
-    <ul>
-        <?php foreach ($person as $key => $value): ?>
-            <li><?= "{$key}: {$value}" ?></li>
-        <?php endforeach; ?>
-    </ul>
->>>>>>> c924b2a9912aba2d28fe19663fe970bc8edde89f
 </body>
 </html>
