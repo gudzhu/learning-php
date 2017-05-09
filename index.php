522131c5596c0edabf1195c9ca4a6dc38e6431a1
gudzhu <gudzhu@users.noreply.github.com>
2017-05-09 19:09:09 +0300

    Added some functions

--- index.php
+++ index.php
@@ -2,16 +2,9 @@
 require 'functions.php';
 require 'Task.php';
 
-try {
-    $pdo = new PDO('mysql:hostname=127.0.0.1;dbname=mytodo', 'root', '68696');
-} catch (PDOException $e) {
-    die($e->getCode() . ': ' . $e->getMessage());
-}
+$pdo = connectToDb();
 
-$statement = $pdo->prepare('select * from todos;');
-$statement->execute();
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-//dd($tasks);
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
 
--- index.view.php
+++ index.view.php
@@ -18,13 +18,15 @@
     <h1>Tasks For The Day</h1>
     <ul>
         <?php foreach ($tasks as $task) : ?>
-            <li>
-                <?php if ($task->completed) : ?>
-                    <strong><?= $task->description; ?></strong>
-                <?php else: ?>
-                    <?= $task->description; ?>
-                <?php endif; ?>
-            </li>
+        <li>
+            <?php if ($task->completed) : ?>
+            <strike>
+                <?= $task->description; ?>
+            </strike>
+            <?php else: ?>
+                <?= $task->description; ?>
+            <?php endif; ?>
+        </li>
         <?php endforeach; ?>
     </ul>
 </body>
