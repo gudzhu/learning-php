8defdabb721f1670276ae398e6a5458ce0fc4f7c
gudzhu <gudzhu@users.noreply.github.com>
2017-07-07 19:01:26 +0300

    commit index.php

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
-	'database' => 'mytodo',
-	'username' => 'root',
-	'password' => '68696',
-	'connection' => 'mysql:host=127.0.0.1',
-	'options' => [
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-	]
+    'database' => [
+        'name' => 'mytodo',
+        'username' => 'root',
+        'password' => '68696',
+        'connection' => 'mysql:host=127.0.0.1',
+        'options' => [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
+    ]
 ];
