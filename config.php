ae09453cd69f7a1656b283180c4f5e6d824312a9
gudzhu <gudzhu@users.noreply.github.com>
2017-07-07 18:05:23 +0300

    commit confir.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'database' => 'mytodo',
+	'username' => 'root',
+	'password' => '68696',
+	'connection' => 'mysql:host=127.0.0.1',
+	'options' => [
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+	]
+];
